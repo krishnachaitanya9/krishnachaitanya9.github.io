efbe0c7aeb7d9b8e9d37f0343d966bd4b0c08b3e
Krishna <chaitanyaradon89@gmail.com>
2020-09-23 16:45:48 -0500

    In the making: Bash script to keep track of latest DL papers

---
+++ update_dl_papers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Start Hardcoding Variables
+ARXIV_PATH="$HOME/PycharmProjects/arXiv-Daily-Mailer/"
+BLOG_PATH="$HOME/PycharmProjects/krishnachaitanya7.github.io/"
+# End hardcoding variables
+git commit -m "Update DL Papers"
+git push -u origin master
