f4924c528b7d538644b31378c7f9c82967fa4e64
Krishna <chaitanyaradon89@gmail.com>
2020-09-25 12:27:50 -0500

    Update DL Papers

---
+++ _posts/DL_Papers.md
@@ -0,0 +1,8 @@
+---
+title: Liquorix Kernel With Panda
+date: 2020-09-25 12:21:02 +0800
+featured-img: pytorch-logo
+categories: [Deep_Learning]
+tags: [Deep_Learning]
+---
+
--- update_dl_papers.sh
+++ update_dl_papers.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 # Start Hardcoding Variables
-ARXIV_PATH="$HOME/PycharmProjects/arXiv-Daily-Mailer/"
+ARXIV_PATH="$HOME/PycharmProjects/arXiv-Daily-Mailer/arxiv_mailer.py"
 BLOG_PATH="$HOME/PycharmProjects/krishnachaitanya7.github.io/"
+ANACONDA_PATH="$HOME/anaconda3/envs/py3.7/bin/python"
+DL_PAPERS_MD="$BLOG_PATH/_posts/DL_Papers.md"
+NOW=$(date +"%Y-%m-%d %H:%M:%S")
 # End hardcoding variables
+cd "$BLOG_PATH"
+echo "---
+title: "Liquorix Kernel With Panda"
+date: $NOW +0800
+featured-img: pytorch-logo
+categories: [Deep_Learning]
+tags: [Deep_Learning]
+---
+" > "DL_PAPERS_MD"
+$ANACONDA_PATH $ARXIV_PATH --print True >> "DL_PAPERS_MD"
+git add .
 git commit -m "Update DL Papers"
 git push -u origin master
