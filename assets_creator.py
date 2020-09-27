aa0e189e9c5697013cdc6486c305667fa44c256f
Krishna <chaitanyaradon89@gmail.com>
2020-09-26 22:34:14 -0500

    modify code to accrately use pictures for posts

--- assets_creator.py
+++ assets_creator.py
@@ -5,10 +5,11 @@ from shutil import copyfile
 if __name__ == "__main__":
     image_file = sys.argv[1]
     image_file_name = image_file.split('.')[0]
+    if "-logo" not in image_file_name:
+        raise Exception("The name should contain logo for example: pytorch-logo.jpg")
     image_file_extension = image_file.split('.')[1]
     file_path = os.path.dirname(os.path.abspath(__file__))
     assets_path = f"{file_path}/assets/img/posts/{image_file}"
     for each_extra_file_name in ['_placehold', '_thumb', '_thumb@2x']:
         dst_file = f"{file_path}/assets/img/posts/{image_file_name}{each_extra_file_name}.{image_file_extension}"
         copyfile(assets_path, dst_file)
-
